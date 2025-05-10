24c451cad20a451aef3a1ad2f8f736c1566d414c
Damian <damianomiotek@outlook.com>
2025-05-10 12:10:32 +0200

    Fix additional empty input

--- front/src/types/chromosome-type.type.ts
+++ front/src/types/chromosome-type.type.ts
@@ -2,9 +2,11 @@ import { ConfigRequest } from "../models/config-request.model";
 
 export type ChromosomeType = "binary" | "real";
 
-export const BinaryChromosomeOptionExclusions: Array<keyof ConfigRequest> =
-  [] as const;
+export const BinaryChromosomeOptionExclusions: Array<keyof ConfigRequest> = [
+  "chromosome_type",
+] as const;
 
 export const RealChromosomeOptionExclusions: Array<keyof ConfigRequest> = [
+  "chromosome_type",
   "inversion_proba",
 ] as const;
